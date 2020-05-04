6f52c793bd98c51966170f3c1cdf56937c6e814c
Pedro Padilha <pedropadilha@me.com>
2020-05-03 23:03:35 -0300

    MLR

--- multinominal-logistic-regression/index.js
+++ multinominal-logistic-regression/index.js
@@ -1,39 +1,45 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
-const loadCsv = require('../load-csv');
 const LogisticRegression = require('./logistic-regression');
 const plot = require('node-remote-plot');
 const _ = require('lodash');
+const mnist = require('mnist-data');
 
-let { features, labels, testFeatures, testLabels } = loadCsv(
-  '../data/cars.csv',
-  {
-    dataColumns: ['horsepower', 'displacement', 'weight'],
-    labelColumns: ['mpg'],
-    shuffle: true,
-    splitTest: 50,
-    converters: {
-      mpg: value => {
-        const mpg = parseFloat(value);
-        if (mpg < 15) {
-          return [1, 0, 0];
-        } else if (mpg < 30) {
-          return [0, 1, 0];
-        } else {
-          return [0, 0, 1];
-        }
-      }
-    }
-  }
-);
+function loadData() {
+  const mnistData = mnist.training(0, 60000);
 
-const regression = new LogisticRegression(features, _.flatMap(labels), {
-  learningRate: 0.5,
-  iterations: 100,
-  batchSize: 50,
-  decisionBoundary: 0.5
+  const features = mnistData.images.values.map(image => _.flatMap(image));
+  const encodedLabels = mnistData.labels.values.map(label => {
+    const row = new Array(10).fill(0);
+    row[label] = 1;
+    return row;
+  });
+
+  return { features, labels: encodedLabels };
+}
+
+const { features, labels } = loadData();
+
+const regression = new LogisticRegression(features, labels, {
+  learningRate: 1,
+  iterations: 40,
+  batchSize: 500
 });
 
 regression.train();
 
-console.log(regression.test(testFeatures, _.flatMap(testLabels)));
+const testMnistData = mnist.testing(0, 10000);
+
+const testFeatures = testMnistData.images.values.map(image => _.flatMap(image));
+const testEncodedLabels = testMnistData.labels.values.map(label => {
+  const row = new Array(10).fill(0);
+  row[label] = 1;
+  return row;
+});
+
+const accuracy = regression.test(testFeatures, testEncodedLabels);
+console.log('Accuracy:', accuracy);
+
+plot({
+  x: regression.costHistory.reverse()
+});
